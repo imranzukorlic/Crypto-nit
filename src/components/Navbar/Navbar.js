ca18ce2eea7c2d99ad03b81c647ead214b178fb2
Imran Zukorlic <mr.silver020@gmail.com>
2022-10-21 17:44:45 +0200

    windov

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -14,7 +14,6 @@ const NavBar = () => {
     <div className="bg-blue-200 sticky top-0 z-20 max-w-screen">
       <nav className="  border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900 sticky bottom-0   ">
         <div className="container flex flex-wrap justify-between items-center pl-[220px]">
-
           <div
             className="flex "
             style={{
@@ -31,7 +30,7 @@ const NavBar = () => {
               CRYPTO-NIT
             </p>
           </div>
-          <ul className="flex flex-col justify-center ml-[100px] p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 w-4/12  mr-64 ">
+          <ul className="flex flex-col justify-center ml-[100px] p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700 w-5/12  mr-64 ">
             <li>
               <NavLink
                 to="/"
--- src/pages/AboutUs/AboutUsPage.js
+++ src/pages/AboutUs/AboutUsPage.js
@@ -5,32 +5,11 @@ const AboutUsPage = () => {
   return (
     <div className="mt-24 mb-20 flex flex-row justify-evenly items-center min-h-[54vh]">
       <UserCard
-        name="Aldin Halilovic"
-        place="Novi Pazar, Serbia"
-        description="Aldin is student of DUNP, and he work's in centar NIT"
-        git="https://github.com/aldinhalilovic"
-        gitimage="https://avatars.githubusercontent.com/u/100434492?v=4"
-      />
-      <UserCard
-        name="Denis Mahmutovic"
-        place="Novi Pazar, Serbia"
-        description="Denis work's in night club and he work's in centar NIT"
-        git="https://github.com/denismahmutovic"
-        gitimage="https://avatars.githubusercontent.com/u/100436234?v=4"
-      />
-      <UserCard
         name="Imran Zukorlic"
         place="Novi Pazar, Serbia"
         description="Imran is student of Medresa, and he work's in centar NIT"
         git="https://github.com/imranzukorlic"
         gitimage="https://avatars.githubusercontent.com/u/100434307?v=4"
-      />
-      <UserCard
-        name="Hamza Belovodjanin"
-        place="Novi Pazar, Serbia"
-        description="Hamza is student of UNINP and he work's in centar NIT"
-        git="https://github.com/hamzabelovodjanin"
-        gitimage="https://avatars.githubusercontent.com/u/100377045?v=4"
       />
     </div>
   );
